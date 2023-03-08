c36c71a69e51ce3d9125776de49aa74606fd6dd5
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2023-03-07 21:47:15 -0800

    Now it tells you what's nearby

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ pygame.init()
 
 # Set the dimensions of the window
 WINDOW_WIDTH = 800
+TEXT_SPACE = 200
 WINDOW_HEIGHT = 600
 
 # Create the window
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT + TEXT_SPACE))
 
 # Set the title of the window
 pygame.display.set_caption("RPG Map")
 
 # Set up the clock
 clock = pygame.time.Clock()
+
+# Set up the font
+font = pygame.font.Font(None, 32)
 
 # Define the directory where the images are stored
 IMAGE_DIR = "images/"
@@ -29,25 +33,16 @@ character_image = pygame.image.load(os.path.join(IMAGE_DIR, "character.png")).co
 character_image = pygame.transform.scale(character_image, (TILE_SIZE, TILE_SIZE))
 
 # Load the terrain images
-terrain_images = []
+terrain_images = {}
 for image_file in os.listdir(IMAGE_DIR):
     if image_file != "character.png":
         terrain_image = pygame.image.load(os.path.join(IMAGE_DIR, image_file)).convert_alpha()
         terrain_image = pygame.transform.scale(terrain_image, (TILE_SIZE, TILE_SIZE))
-        terrain_images.append(terrain_image)
+        terrain_images[image_file[:-4]] = terrain_image
 
 # Define the number of tiles in each direction
 NUM_TILES_X = int(WINDOW_WIDTH / TILE_SIZE)
 NUM_TILES_Y = int(WINDOW_HEIGHT / TILE_SIZE)
-
-# Define the map
-map_tiles = []
-for x in range(NUM_TILES_X):
-    row = []
-    for y in range(NUM_TILES_Y):
-        terrain_tile = random.choice(terrain_images)
-        row.append(terrain_tile)
-    map_tiles.append(row)
 
 # Define the character's position
 character_x = int(NUM_TILES_X / 2)
@@ -70,7 +65,7 @@ for x in range(NUM_TILES_X):
     row = []
     for y in range(NUM_TILES_Y):
         if random.random() < SPARSE_FACTOR:
-            terrain_tile = random.choice(terrain_images)
+            terrain_tile = random.choice(list(terrain_images.keys()))
         else:
             terrain_tile = None
         row.append(terrain_tile)
@@ -110,16 +105,30 @@ while running:
     # Black out the screen
     screen.fill((0, 0, 0))
 
+    # Track what is on screen
+    on_screen = []
+
     # Draw the map
     for x in range(view_x, view_x2):
         for y in range(view_y, view_y2):
-            tile_image = map_tiles[x][y]
-            if tile_image is not None:
+            image_name = map_tiles[x][y]
+            if image_name is not None:
+                tile_image = terrain_images[image_name]
+                on_screen.append(map_tiles[x][y])
                 screen.blit(tile_image, ((x - view_x) * TILE_SIZE, (y - view_y) * TILE_SIZE))
 
     # Draw the character
     character_position = ((character_x - view_x) * TILE_SIZE, (character_y - view_y) * TILE_SIZE)
     screen.blit(character_image, character_position)
+
+    # Draw the text
+    texts = ["Things that are nearby:", ", ".join(on_screen), "", "Press Q to quit, arrow keys to move"]
+    y = WINDOW_HEIGHT + 20
+    for line in texts:
+        text = font.render(line, True, (255, 255, 255))
+        text_rect = text.get_rect(topleft=(20, y))
+        screen.blit(text, text_rect)
+        y += 32
 
     # Update the screen
     pygame.display.flip()
