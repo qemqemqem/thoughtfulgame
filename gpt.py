40c7b3f1f6a1fb24251a30ee4e4e8a99dba96ff6
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2023-03-11 10:02:26 -0800

    Change model

--- gpt.py
+++ gpt.py
@@ -12,7 +12,8 @@ def prompt_completion(question):
     start_time = time.perf_counter()
     prompt = f"{question} "
     response = openai.Completion.create(
-        engine="curie", # "curie" is cheaper, "davinci" is good, there's also an option to get chatgpt on the website
+        # https://openai.com/blog/introducing-chatgpt-and-whisper-apis suggests "gpt-3.5-turbo", but my API key doesn't work for that
+        engine="davinci-instruct-beta", # "curie" is cheaper, "davinci" is good, there's also an option to get chatgpt on the website
         prompt=prompt,
         max_tokens=64,
         n=1,
@@ -22,7 +23,7 @@ def prompt_completion(question):
     answer = response.choices[0].text.strip()
     # print(f"\tPROMPT: {question}\n\tANSWER: {answer}\n")
     duration = time.perf_counter() - start_time
-    print("Duration: {:.2f} seconds".format(duration))
+    print(f"Duration: {duration:.2f} seconds: {answer}")
     return answer
 
 
